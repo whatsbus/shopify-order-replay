3869dfbafd9f3a280c49ba362de02e387aae71fb
Football <whatsbuss1@gmail.com>
2026-06-29 20:59:00 +0100

    Update types.ts

--- src/jobs/types.ts
+++ src/jobs/types.ts
@@ -1,1 +1,40 @@
+// src/jobs/types.ts
 
+export type JobType =
+  | "initial_sync"
+  | "refresh_orders"
+  | "replay_orders"
+  | "register_webhooks"
+
+export interface BaseJob<T = unknown> {
+  id: string
+  type: JobType
+  shopId: number
+  shopDomain: string
+  payload: T
+  createdAt: number
+  attempts: number
+  maxAttempts: number
+}
+
+export interface InitialSyncPayload {
+  accessToken: string
+}
+
+export interface RefreshOrdersPayload {
+  accessToken: string
+}
+
+export interface ReplayOrdersPayload {
+  orderIds?: number[]
+}
+
+export interface RegisterWebhooksPayload {
+  accessToken: string
+}
+
+export type Job =
+  | BaseJob<InitialSyncPayload>
+  | BaseJob<RefreshOrdersPayload>
+  | BaseJob<ReplayOrdersPayload>
+  | BaseJob<RegisterWebhooksPayload>
