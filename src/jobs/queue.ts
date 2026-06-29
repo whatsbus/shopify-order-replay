a3ad00e9e7ca8972e34f90900661335ace18c9e0
Football <whatsbuss1@gmail.com>
2026-06-29 20:47:53 +0100

    Update queue.ts

--- src/jobs/queue.ts
+++ src/jobs/queue.ts
@@ -1,1 +1,8 @@
+import { Queue } from "bullmq"
+import IORedis from "ioredis"
 
+const connection = new IORedis(process.env.REDIS_URL!)
+
+export const orderSyncQueue = new Queue("order-sync", {
+  connection,
+})
