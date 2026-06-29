da4aece8587d489a443482df06f8a65e8f6bdf2f
Football <whatsbuss1@gmail.com>
2026-06-29 20:46:44 +0100

    Update worker.ts

--- src/jobs/worker.ts
+++ src/jobs/worker.ts
@@ -1,1 +1,17 @@
+import { Worker } from "bullmq"
+import IORedis from "ioredis"
+import { syncOrders } from "../services/orderSync.js"
 
+const connection = new IORedis(process.env.REDIS_URL!)
+
+export const orderSyncWorker = new Worker(
+  "order-sync",
+  async (job) => {
+    const { shopId, shopDomain, accessToken } = job.data
+
+    await syncOrders(shopId, shopDomain, accessToken)
+  },
+  { connection }
+)
+
+console.log("[worker] Order sync worker running...")
